1ddd4b0469f423acd4802702ce3f94277b6a188f
aim4dabush <edarasato@gmail.com>
2022-05-24 10:52:40 -0700

    first commit

---
+++ src/Variables/JS.js
@@ -0,0 +1,12 @@
+//colors
+export const white = "#ffffff";
+export const cyan = "#62e0d9";
+export const black = "#000000";
+export const darkBlueOne = "#1b2330";
+export const darkBlueTwo = "#202a3c";
+export const darkBlueThree = "#181f2b";
+export const darkBlueFour = "#0b1524";
+export const lightBlue = "#8adae3";
+export const offWhite = "#dfe4f0";
+export const blue = "#07043b";
+export const red = "#ff4242";
